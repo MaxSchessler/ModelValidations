90f3ce6572efc5159554490b04ba8d1448727de1
sches <sches@XPS>
2024-11-24 13:47:06 -0600

    Add project files.

---
+++ ModelValidations/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ModelValidations.Models;
+
+namespace ModelValidations.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            if (ModelState.IsValid == false) 
+                return BadRequest(new BadRequestModel(ModelState, ModelState.ErrorCount));
+            return Content($"{person}");
+        }
+    }
+}
+
+
+public record BadRequestModel(ModelStateDictionary ModelState, int NumberOfErrors);
---
+++ ModelValidations/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidations.Models
+{
+    public class Person
+    {
+        [Required]
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone {  get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword  { get; set; }
+        public double? Price { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Email: {Email}, Phone: {Phone}, Password {Password}, " +
+                $"ConfirmPassword: {ConfirmPassword}, Price: {Price}.";
+        }
+    }
+}
