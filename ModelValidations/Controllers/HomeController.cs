9a25906d1bcebd78d350409ff9e070ae1c254aff
Max Schessler <schessler.max@gmail.com>
2024-11-25 11:19:01 -0600

    New endpoints + edit to Main endpoint

--- ModelValidations/Controllers/HomeController.cs
+++ ModelValidations/Controllers/HomeController.cs
@@ -9,9 +9,35 @@ namespace ModelValidations.Controllers
         [Route("register")]
         public IActionResult Index(Person person)
         {
-            if (ModelState.IsValid == false) 
-                return BadRequest(new BadRequestModel(ModelState, ModelState.ErrorCount));
-            return Content($"{person}");
+            if (!ModelState.IsValid)
+            {
+                string errors = string.Join("\n",
+                    ModelState.Values
+                    .SelectMany(value => value.Errors)
+                    .Select(err => err.ErrorMessage));
+
+                return BadRequest(errors);
+            } else
+            {
+                return Ok(person);
+            }            
+        }
+
+        [Route("test-user")]
+        public Person Details()
+        {
+            Person person = new Person()
+            {
+                Name = "Max Schessler",
+                Email = "email@email.com",
+                Password = "password",
+                ConfirmPassword = "password",
+                Phone = "123-456-7890",
+                Price = 150.00,
+                DOB = new DateTime(2005, 12, 21),
+            };
+
+            return person;
         }
     }
 }
