d4535215b724145cd247642d80136cdee31afc91
Max Schessler <schessler.max@gmail.com>
2024-11-25 11:02:55 -0600

    Added customer validator

---
+++ ModelValidations/Validations/MinimumAge.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidations.Validations
+{
+    public class MinimumAge : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                DateTime date = (DateTime)value;
+                DateTime now = DateTime.Now;
+                if (date.Year - now.Year == 0)
+                {
+                    return ValidationResult.Success;
+                } else
+                {
+                    return new ValidationResult("Must be over 18 years ago.")
+                }
+            }
+        }
+    }
+}
