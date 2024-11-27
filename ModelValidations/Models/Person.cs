38b8cb052031b624e84ab694d4170aca88a4230f
Max Schessler <schessler.max@gmail.com>
2024-11-26 22:53:54 -0600

    Implemented Validate method from System.ComponentModel.DataAnnotations.IValidatableObject

--- ModelValidations/Models/Person.cs
+++ ModelValidations/Models/Person.cs
@@ -1,9 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 using ModelValidations.Validations;
 
 namespace ModelValidations.Models
 {
-    public class Person
+    public class Person : IValidatableObject
     {
         [Required(ErrorMessage = "{0} can't be empty or null.")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "{0} must have 6 - 100 characters.")]
@@ -15,7 +16,7 @@ namespace ModelValidations.Models
         public string? ConfirmPassword  { get; set; }
         public double? Price { get; set; }
 
-        [MinimumAge(ErrorMessage = "DOB must be 18 years ago.")]
+        //[MinimumAge(ErrorMessage = "DOB must be 18 years ago.")]
         public DateTime? DOB { get; set; }
 
 
@@ -23,6 +24,22 @@ namespace ModelValidations.Models
         {
             return $"Name: {Name}, Email: {Email}, Phone: {Phone}, Password {Password}, " +
                 $"ConfirmPassword: {ConfirmPassword}, Price: {Price}.";
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (DOB.HasValue)
+            {
+                DateTime now = DateTime.Now;
+                if (now.Year - DOB.Value.Year <= 18)
+                    yield return new ValidationResult("Person is not 18 years or older.");
+            }
+
+            if (Phone != null && Phone.Length > 0) 
+            {
+                if (Regex.IsMatch(Phone, @"^(\+[0-9])$") == false) 
+                    yield return new ValidationResult($"{Phone} is not valid.");
+            }
         }
     }
 }
